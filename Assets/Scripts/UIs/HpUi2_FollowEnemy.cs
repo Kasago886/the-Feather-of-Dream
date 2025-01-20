158282ace7c9166001f7a9b73a2122e933706346
X <hang_06214@163.com>
2025-01-20 10:26:52 +0800

    伴随血条

--- Assets/Scripts/UIs/HpUi2_FollowEnemy.cs
+++ Assets/Scripts/UIs/HpUi2_FollowEnemy.cs
@@ -5,79 +5,99 @@ using UnityEngine.UI;
 
 public class HpUi2_FollowEnemy : MonoBehaviour
 {
+    public GameObject HpPrefab;
     private Enemy enemy;
     private GameObject scrollView, viewPort, content;
     private ContentSizeFitter contentSizeFitter;
-    private RectTransform scrollViewRectTransform,viewPortRectTransform, contentRectTransform;
+    private RectTransform scrollViewRectTransform, viewPortRectTransform, contentRectTransform;
     private ScrollRect scrollRect;
     private VerticalLayoutGroup layoutGroup;
-    private int limit;
+    private SpriteRenderer spriteRenderer;
+    private Sprite sprite;
+    private Dictionary<HpUI,Feather> hpDic;
     void Start()
     {
         enemy = GetComponent<Enemy>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            sprite = spriteRenderer.sprite;
+        }
+        scrollView = new GameObject(enemy.enemyName + "TemporaryEnemyHealthUi", typeof(ScrollRect));
+        scrollView.AddComponent<Scroll>();
+        viewPort = new GameObject("EnemyHealthViewPort", typeof(RectMask2D));
+        content = new GameObject("HealthUi", typeof(RectTransform), typeof(ContentSizeFitter), typeof(LayoutGroup));
+        scrollViewRectTransform = scrollView.GetComponent<RectTransform>();
+        viewPortRectTransform = viewPort.GetComponent<RectTransform>();
+        contentRectTransform = content.GetComponent<RectTransform>();
+        scrollViewRectTransform.SetParent(GameObject.FindGameObjectWithTag("Canvas").GetComponent<RectTransform>());
+        viewPortRectTransform.SetParent(scrollViewRectTransform);
+        contentRectTransform.SetParent(viewPortRectTransform);
+        scrollViewRectTransform.sizeDelta = new Vector2(90, 120);
+        viewPortRectTransform.sizeDelta = new Vector2(90, 120);
+        viewPortRectTransform.anchorMin = Vector2.zero;
+        viewPortRectTransform.anchorMax = Vector2.one;
+        viewPortRectTransform.pivot = new Vector2(0.5f, 0);
+        contentRectTransform.anchorMax = Vector2.zero;
+        contentRectTransform.anchorMin = Vector2.zero;
+        contentRectTransform.pivot = Vector2.zero;
+        contentRectTransform.sizeDelta = new Vector2(90, 0);
+        contentSizeFitter = content.GetComponent<ContentSizeFitter>();
+        layoutGroup = content.GetComponent<VerticalLayoutGroup>();
+        scrollRect = scrollView.GetComponent<ScrollRect>();
+        //水平任意
+        contentSizeFitter.horizontalFit = ContentSizeFitter.FitMode.Unconstrained;
+        //垂直调整
+        contentSizeFitter.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
+        //物体之间间距
+        layoutGroup.spacing = 10;
+        //物体顶部间距
+        layoutGroup.padding.top = 10;
+        //物体底部间距
+        layoutGroup.padding.bottom = 10;
+        //分配身份
+        scrollRect.content = contentRectTransform;
+        scrollRect.viewport = viewPortRectTransform;
+        enemy.hpScroll = scrollView.GetComponent<Scroll>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (limit < 1)
+        if (sprite != null)
         {
-            limit++;
-            scrollView=new GameObject(enemy.enemyName+"TemporaryEnemyHealthUi",typeof(ScrollRect));
-            viewPort = new GameObject("EnemyHealthViewPort", typeof(RectMask2D));
-            content = new GameObject("HealthUi",typeof(RectTransform),typeof(ContentSizeFitter),typeof(LayoutGroup));
-            scrollViewRectTransform = scrollView.GetComponent<RectTransform>();
-            viewPortRectTransform = viewPort.GetComponent<RectTransform>();
-            contentRectTransform = content.GetComponent<RectTransform>();
-            scrollViewRectTransform.SetParent(GameObject.FindGameObjectWithTag("Canvas").GetComponent<RectTransform>());
-            viewPortRectTransform.SetParent(scrollViewRectTransform);
-            contentRectTransform.SetParent(viewPortRectTransform);
-            scrollViewRectTransform.sizeDelta = new Vector2(90, 120);
-            viewPortRectTransform.sizeDelta = new Vector2(90, 120);
-            viewPortRectTransform.anchorMin = Vector2.zero;
-            viewPortRectTransform.anchorMax = Vector2.one;
-            viewPortRectTransform.pivot = new Vector2(0.5f, 0);
-            contentRectTransform.anchorMax= Vector2.zero;
-            contentRectTransform.anchorMin= Vector2.zero;
-            contentRectTransform.pivot= Vector2.zero ;
-            contentRectTransform.sizeDelta = new Vector2(90, 0);
-            contentSizeFitter = content.GetComponent<ContentSizeFitter>();
-            layoutGroup = content.GetComponent<VerticalLayoutGroup>();
-            scrollRect = scrollView.GetComponent<ScrollRect>();
-            //水平任意
-            contentSizeFitter.horizontalFit = ContentSizeFitter.FitMode.Unconstrained;
-            //垂直调整
-            contentSizeFitter.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
-            //物体之间间距
-            layoutGroup.spacing = 10;
-            //物体顶部间距
-            layoutGroup.padding.top = 10;
-            //物体底部间距
-            layoutGroup.padding.bottom = 10;
-            //分配身份
-            scrollRect.content = contentRectTransform;
-            scrollRect.viewport = viewPortRectTransform;
+            scrollViewRectTransform.position = Camera.main.WorldToScreenPoint(new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + sprite.rect.height / 2, 0));
         }
-        if(limit==1)
+        AddHpUi();
+        Controller();
+        
+    }
+    void AddHpUi()
+    {
+        if (enemy.unlockedFeathers.Count > content.transform.childCount)
         {
-            scrollViewRectTransform.position=Camera.main.WorldToScreenPoint(gameObject.transform.position);
-
+            GameObject hpUiObject = Instantiate(HpPrefab);
+            RectTransform transform = hpUiObject.GetComponent<RectTransform>();
+            transform.SetParent(contentRectTransform);
+            HpUI hpUI = hpUiObject.GetComponent<HpUI>();
+            hpUI.testHpMax = enemy.unlockedFeathers[content.transform.childCount - 1].maxHealth;
+            hpUI.testHp = enemy.unlockedFeathers[content.transform.childCount - 1].health;
+            hpUI.testTime = 1;
+            hpDic.Add(hpUI, enemy.unlockedFeathers[content.transform.childCount - 1]);
         }
     }
-    private Image AddHealthUi()
+    void Controller()
     {
-        if(content!=null)
+        foreach (var hp in hpDic)
         {
-            GameObject health = new GameObject("HealthBar", typeof(RectTransform), typeof(Image));
-            RectTransform healthRectTransform=health.GetComponent<RectTransform>();
-            Image image = healthRectTransform.GetComponent<Image>();
-            healthRectTransform.SetParent(contentRectTransform);
-            healthRectTransform.sizeDelta = new Vector2(90, 9);
-            image.color = Color.red;
-            image.type = Image.Type.Filled;
-            image.fillMethod=Image.FillMethod.Horizontal;
-            return image;
+            if (hp.Value != null)
+            {
+                hp.Key.testHp = hp.Value.health;
+            }
+            else
+            {
+                hpDic.Remove(hp.Key);
+            }
         }
-        return null;    
     }
 }
