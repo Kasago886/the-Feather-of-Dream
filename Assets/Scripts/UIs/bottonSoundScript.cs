95db5173f77406dadcafee8ab0c12b58f65c61f3
Kasago886 <2070447675@qq.com>
2025-01-21 17:14:38 +0800

    音效

---
+++ Assets/Scripts/UIs/bottonSoundScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class bottonSoundScript : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler
+{
+    AudioSource source;
+    Button button;
+    bool isbutton = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = gameObject.GetComponent<AudioSource>();
+        button = gameObject.GetComponent<Button>();
+
+        if (button != null)
+        {
+            isbutton = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)    //鼠标移入
+    {
+        if (isbutton)
+        {
+            if (button.interactable)
+            {
+                source.Play();
+            }
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)    //按钮按下
+    {
+        if (isbutton)
+        {
+            if (button.interactable)
+            {
+                source.Play();
+            }
+        }
+    }
+}
