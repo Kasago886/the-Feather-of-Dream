133f3b5704d812a1d336166cbdcee646db87fd9c
Kasago886 <2070447675@qq.com>
2025-02-09 20:26:13 +0800

    UI

--- Assets/Scripts/Consts.cs
+++ Assets/Scripts/Consts.cs
@@ -11,6 +11,8 @@ public class Consts
     public const string GroundTag = "Ground";
     public const string CardGenerateTextTag = "CardGenerateText";
     public const string CardPanelTag = "CardPanel";
+    public const string BGMSliderTag = "BGMSlider";
+    public const string EffectSliderTag = "EffectSlider";
 
     public const string PlayerLayer = "Player";
     public const string EnemyLayer = "Enemy";
